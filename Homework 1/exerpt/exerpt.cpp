5ceccd8f51963ad7aa7254170148349513dd9a25
Lisa Jacklin <lisaloveslola09@outlook.com>
2022-01-19 16:52:15 -0900

    code 0 w/ no quotes

--- Homework 1/exerpt/exerpt.cpp
+++ Homework 1/exerpt/exerpt.cpp
@@ -1,20 +1,20 @@
-// exerpt.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+//exerpt.cpp 
+//Lisa Jacklin
+//CS 202
+//1/17/2022
+
+//Requirement: write a program that prints an excerpt
+//from a Project Gutenberg book.
 
 #include <iostream>
+using std::cout;
+using std::endl;
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    //quote from Charles Dickens, A tale of two cities
+    //then tell wind and fire where to stop," returned madame; "but don't tell me."
+    std::cout << "Then tell Wind and Fire where to stop, returned madame; but don't tell me.";
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
